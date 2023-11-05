ddaee0a67048858558c175d033b3e9ef6823ef8b
Sugalya10 <149962158+Sugalya10@users.noreply.github.com>
2023-11-06 03:22:06 +0530

    Add files via upload

---
+++ MP1_Madlibs.py
@@ -0,0 +1,65 @@
+#MADLIBS_GAME_MP1
+#line 1
+N1=input("Noun 1 =")
+A1=input("Adjective 1 =")
+N2=input("Noun 2 =")
+N3=input("Noun 3 =")
+#line2
+V1=input("Verb 1 =")
+N4=input("Noun 4 =")
+#line 3
+V2=input("Verb 2 =")
+V3=input("Verb 3 =")
+#line 4
+N5=input("Noun 5 =")
+#line 5
+N6=input("Noun 6 =")
+N7=input("Noun 7 =")
+N8=input("Noun 8 =")
+#line 6
+N9=input("Noun 9 =")
+N10=input("Noun 10 =")
+N11=input("Noun 11 =")
+N12=input("Noun 12 =")
+#line 7
+V4=input("Verb 4 =")
+V5=input("Verb 5 =")
+#line 8
+N13=input("Noun 13 =")
+N14=input("Noun 14 =")
+A2=input("Adjective 2 =")
+#line 9
+V6=input("Verb 6 =")
+N15=input("Noun 15 =")
+#line 10
+A2=input("Adjective 2 =")
+V7=input("Verb 7 =")
+#line 11
+A3=input("Adjective 3 =")
+A4=input("Adjective 4 =")
+N16=input("Noun 16 =")
+N17=input("Noun 17 =")
+
+#Storyline 
+
+print("Harry Potter-An outline")
+
+print("")
+
+Story=f"Harry Potter was born in England,{N1}. His parents, Lily and James Potter, were killed by an {A1} {N2} named Lord {N3},\
+while Voldemort was trying to {V1} Harry. Yet Harry escaped with no harm except a scar in the shape of a {N4}, and Voldemort\
+ends up mostly {V2}. Harry is raised by his non-magical Aunt Petunia, Uncle Vernon, and cousin (Dudley), and is {V3} by them.\
+He believes the false words of the Dursleys that Harry's parents were killed in a {N5}. As a child, he did not know that he\
+was a {N6}. He eventually discovers that there are many magical {N7}, living secretly and hiding from {N8}. When Harry\
+turned eleven on July 31, he received a {N9} inviting him to go to a school called {N10} for young {N11} and {N12}.\
+Each book tells the story of one year of his life at the school and tells how he {V4}. On Hogwarts Express, the {V5}\
+they use to get to Hogwarts, he meets {N13} and {N14}, who become his {A2}. Voldemort tries several times to return\
+to life and {V6} Harry. In the fourth book, Voldemort comes to life with help from his follower, a Marauder, {N15}.\
+The series ends with an {V7} battle between Harry and Lord Voldemort and a final epilogue that shows Harry and his\
+friends {A3} and {A4} with their {N16} and {N17}."
+
+
+print(Story)
+
+
+
